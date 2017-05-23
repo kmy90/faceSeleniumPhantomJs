ab3d11945068e11c4cf2491785d18852d11e5729
Camilo Fonseca <camilof@oktana.io>
2017-05-23 12:41:44 -0300

    --

--- index.js
+++ index.js
@@ -74,52 +74,51 @@ var By = webdriver.By;
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+
 app.get('/', function(request, response) {
     response.send('Hello World!')
 })
+
 app.get('/test', function(request, response) {
-        response.status(200).send('test');
-    })
-    /*
-    app.post('/sendMessage', function(request, response) {
-        //Get Credentials
-        var username = request.body.username;
-        var password = request.body.password;
-        var recipientId = request.body.recipientId;
-        var message = request.body.message;
-      
-        var  driver = new webdriver.Builder()
-            .forBrowser('phantomjs')
-            .build();
+    response.status(200).send('test');
+})
 
-        driver.get('https://facebook.com/login/');
-        driver.wait(function() {
-            return driver.getTitle().then(function(title) {
-                //Login User
-                driver.findElement(By.xpath("//input[@id='email']")).sendKeys(username);
-                driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(password);
-                driver.findElement(By.xpath("//*[@id='loginbutton']")).click();
+app.post('/sendMessage', function(request, response) {
+    //Get Credentials
+    //  var username = request.body.username;
+    // var password = request.body.password;
+    //   var recipientId = request.body.recipientId;
+    //   var message = request.body.message;
 
-                //Access to messenger directly to page to write
-                driver.get('https://www.facebook.com/messages/t/' + recipientId);
-                return true;
-            });
-        }, 5000).then(function() {
-            //Write the message and press Return
-            driver.findElement(By.xpath("//*[@contenteditable='true' and @role='combobox']")).sendKeys(message, webdriver.Key.RETURN);
-            response.status(201).send('Done');
-        }, function(error) {
-            //future send error to org
-            response.status(500).send(error);
-        });
+    var driver = new webdriver.Builder()
+        .forBrowser('phantomjs')
+        .build();
 
-        //Close the test
-        driver.quit();
-    });
+    //  driver.get('https://facebook.com/login/');
+    // driver.wait(function() {
+    //   return driver.getTitle().then(function(title) {
+    //Login User
+    //       driver.findElement(By.xpath("//input[@id='email']")).sendKeys(username);
+    //     driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(password);
+    //     driver.findElement(By.xpath("//*[@id='loginbutton']")).click();
 
-    */
+    //Access to messenger directly to page to write
+    //  driver.get('https://www.facebook.com/messages/t/' + recipientId);
+    //  return true;
+    //   });
+    //  }, 5000).then(function() {
+    //Write the message and press Return
+    //  driver.findElement(By.xpath("//*[@contenteditable='true' and @role='combobox']")).sendKeys(message, webdriver.Key.RETURN);
+    //    response.status(201).send('Done');
+    //  }, function(error) {
+    //future send error to org
+    //     response.status(500).send(error);
+    // });
 
-
+    //Close the test
+    // driver.quit();
+    response.status(201).send('Done');
+});
 
 
 app.listen(app.get('port'), function() {
