05592633a66455cd5b4f31e46fa979a79cf90de2
felParodi <felipe.parodi91@gmail.com>
2017-05-24 15:47:20 -0300

    aaa

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var webdriver = require('selenium-webdriver')
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-var direverBuider = new webdriver.Builder().forBrowser('phantomjs');
-
+//var direverBuider = new webdriver.Builder().forBrowser('phantomjs');
+var direverBuider = new webdriver.Builder().forBrowser('chrome');
 app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
@@ -14,61 +14,24 @@ var By = webdriver.By;
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-
-app.get('/', function(request, response) {
-    response.send('Hello World!')
-})
-
-app.get('/testA', function(req, res) {
-    var driver = new webdriver.Builder()
-        .forBrowser('phantomjs')
-        .build();
-    driver.get('http://www.google.com/ncr');
-    driver.findElement(By.name('q')).sendKeys('webdriver', webdriver.Key.ENTER);
-    //driver.findElement(By.name('btnG')).click();
-    driver.wait(function() {
-        return driver.getTitle().then(function(title) {
-            console.log(title);
-            return title === 'webdriver - Google Search';
-        });
-    }, 5000).then(function() {
-        res.status(200).send('Done');
-    }, function(error) {
-        res.status(200).send(error);
-    });
-    driver.quit();
-});
-
-
-app.get('/error', function(request, response) {
-    try {
-        var u = undefined;
-        u.a.b();
-        response.status(200).send("done");
-    } catch (e) {
-        response.status(500).send(JSON.stringify(e));
-    }
-});
-
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
 })
 
 app.post('/sendMessage', (request, response) => {
     //Get Credentials
-    let responseSed = false;
+    let responseSent = false;
     try {
         var username = request.body.username;
         var password = request.body.password;
         var recipientId = request.body.recipientId;
         var message = request.body.message;
-
         var driver = direverBuider.build();
         let error = (e) => {
-            if(!responseSed) {
+            if(!responseSent) {
                 console.error('Error', e);
                 response.status(412).send(e);
-                responseSed = true;
+                responseSent = true;
             }
         }
         //Login
@@ -84,26 +47,28 @@ app.post('/sendMessage', (request, response) => {
         }, 2000).catch(error);
         //Access to messenger directly to page to write
         driver.findElement(By.xpath("//*[@contenteditable='true' and @role='combobox']")).sendKeys(message, webdriver.Key.ENTER).catch(error);
-        /*
+
         //Close Account
-        driver.findElement(By.xpath("//*[@id='userNavigationLabel']")).click().catch(error);;
-        var logoutKey = '"{"ref":"async_menu","logout_menu_click":"menu_logout"}';
-        driver.findElement(By.xpath("//a[@data-gt='" + logoutKey + "']")).click().catch(error);;
-
-
+        /*
+        driver.sleep(10000);
+        driver.findElement(By.xpath("//a[@aria-labelledby='userNavigationLabel']")).click().catch(error);;
+        var logoutKey = '{"ref":"async_menu","logout_menu_click":"menu_logout"}';
+        driver.findElement(By.xpath("//a[@data-gt='{\"ref\":\"async_menu\",\"logout_menu_click\":\"menu_logout\"}']")).click().catch(error);;
         //*/
+        /*
         driver.quit().then(()=>{
-            if(!responseSed) {
+            if(!responseSent) {
                 response.status(200).send('Done');
-                responseSed = true;
+                responseSent = true;
             }
         },error);
+        //*/
         //response.status(200).send('Done');
     } catch (e) {
         console.log(e);
-        if(!responseSed) {
+        if(!responseSent) {
             response.status(500).send(JSON.stringify(e));
-            responseSed = true;
+            responseSent = true;
         }
     }
 });
