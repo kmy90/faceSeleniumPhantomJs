7f2c3960ef15b4d0c1f909a68ecba95c2cfbf43c
Camilo Fonseca <camilof@oktana.io>
2017-05-25 12:09:16 -0300

    Fix issues

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var webdriver = require('selenium-webdriver')
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-//var direverBuider = new webdriver.Builder().forBrowser('phantomjs');
-var direverBuider = new webdriver.Builder().forBrowser('chrome');
+var direverBuider = new webdriver.Builder().forBrowser('phantomjs');
+//var direverBuider = new webdriver.Builder().forBrowser('chrome');
 app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
@@ -28,15 +28,15 @@ app.post('/sendMessage', (request, response) => {
         var message = request.body.message;
         var driver = direverBuider.build();
         let error = (e) => {
-            if(!responseSent) {
-                console.error('Error', e);
-                response.status(412).send(e);
-                responseSent = true;
+                if (!responseSent) {
+                    console.error('Error', e);
+                    response.status(412).send(e);
+                    responseSent = true;
+                }
             }
-        }
-        //Login
+            //Login
         driver.get('https://www.facebook.com/messages/t/' + recipientId).catch(error);
-       //Write the message and press Return
+        //Write the message and press Return
         driver.findElement(By.xpath("//input[@id='email']")).sendKeys(username).catch(error);
         driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(password).catch(error);
         driver.findElement(By.xpath("//*[@id='loginbutton']")).click().catch(error);
@@ -48,25 +48,15 @@ app.post('/sendMessage', (request, response) => {
         //Access to messenger directly to page to write
         driver.findElement(By.xpath("//*[@contenteditable='true' and @role='combobox']")).sendKeys(message, webdriver.Key.ENTER).catch(error);
 
-        //Close Account
-        /*
-        driver.sleep(10000);
-        driver.findElement(By.xpath("//a[@aria-labelledby='userNavigationLabel']")).click().catch(error);;
-        var logoutKey = '{"ref":"async_menu","logout_menu_click":"menu_logout"}';
-        driver.findElement(By.xpath("//a[@data-gt='{\"ref\":\"async_menu\",\"logout_menu_click\":\"menu_logout\"}']")).click().catch(error);;
-        //*/
-        /*
-        driver.quit().then(()=>{
-            if(!responseSent) {
-                response.status(200).send('Done');
+        driver.quit().then(() => {
+            if (!responseSent) {
+                response.status(201).send('Done');
                 responseSent = true;
             }
-        },error);
-        //*/
-        //response.status(200).send('Done');
+        }, error);
     } catch (e) {
         console.log(e);
-        if(!responseSent) {
+        if (!responseSent) {
             response.status(500).send(JSON.stringify(e));
             responseSent = true;
         }
