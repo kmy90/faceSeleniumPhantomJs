5b09348e5fb51d78317538a0cb7bcc5bc515c0bd
felParodi <felipe.parodi91@gmail.com>
2017-06-01 16:59:38 -0300

    .

--- dist/router/dbRouter.js
+++ dist/router/dbRouter.js
@@ -14,6 +14,12 @@ class DBRouter {
                 dbc.close();
             });
         });
+        this.router.post('/:colec', (req, res) => {
+            db_1.DBConection.init().then((dbc) => {
+                dbc.insertCollectionOne(req.params.colec, req.body).then((e) => res.status(200).send(e), (e) => res.status(505).send(e));
+                dbc.close();
+            });
+        });
         this.router.get('/:colec/drop', (req, res) => {
             db_1.DBConection.init().then((dbc) => {
                 dbc.removeCollectionMany(req.params.colec, {}).then((e) => res.status(200).send(e), (e) => res.status(505).send(e));
--- src/router/dbRouter.ts
+++ src/router/dbRouter.ts
@@ -18,6 +18,12 @@ export class DBRouter {
                 dbc.close()
             });
         });
+        this.router.post('/:colec',(req,res)=>{
+            DBConection.init().then((dbc)=>{
+                dbc.insertCollectionOne(req.params.colec, req.body).then((e)=>res.status(200).send(e), (e)=>res.status(505).send(e));
+                dbc.close()
+            });
+        });
         this.router.get('/:colec/drop',(req,res)=>{
             DBConection.init().then((dbc) => {
                 dbc.removeCollectionMany(req.params.colec,{}).then((e)=>res.status(200).send(e), (e)=>res.status(505).send(e));
